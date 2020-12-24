2fc3305f2014e51e51f9523c99cb667896d40338
Bart McPherson <b.mcpherson@csuohio.edu>
2020-12-24 16:55:20 -0500

    ignore config.js and include a default one

---
+++ src/utils/default.config.js
@@ -0,0 +1,15 @@
+/* const config = {
+*  stationname: 'WCSB',
+*  username: 'adminuser',
+*  userpassword: 'Ar3allyG00dPazzw0rd',
+*  stream:
+*    {
+*      name: 'High Bit-Rate Stream',
+*      ip: '127.0.0.1',
+*      port: 1234,
+*      uri: '/admin.cgi?sid=1&mode=viewjson&page=3'
+*    }
+*}
+*
+* module.exports = config
+*/
