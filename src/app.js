32b44ad35a8bd26c57a2b6fcdec8734cb111454a
Bart McPherson <b.mcpherson@csuohio.edu>
2021-01-10 16:14:30 -0500

    add dev and start script settings. set port to 4000 instead of 3000

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const shoutcast = require('./utils/shoutcast')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.get('', (req, res) => {
   shoutcast((error, listenerData) => {
