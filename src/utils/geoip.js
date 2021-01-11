d3c82e296b6be61efcc09cec800be1d6e085bb3b
Bart McPherson <b.mcpherson@csuohio.edu>
2021-01-10 19:28:05 -0500

    change geoip service

--- src/utils/geoip.js
+++ src/utils/geoip.js
@@ -1,7 +1,8 @@
 const request = require('postman-request')
 
 const geoInfo = (ip, callback) => {
-  const url = 'http://getcitydetails.geobytes.com/GetCityDetails?fqcn=' + ip
+  const url = 'https://tools.keycdn.com/geo.json?host=' + ip
+  //const url = 'http://getcitydetails.geobytes.com/GetCityDetails?fqcn=' + ip
 
   request({url, json: true}, (error, response, body) => {
     if(error) {
@@ -10,8 +11,13 @@ const geoInfo = (ip, callback) => {
       const geoObject = {}
       //geoObject.location = body
       console.log(body)
-      geoObject.location = body.geobytesfqcn
-      geoObject.country = body.geobytesinternet
+      //geoObject.location = body.geobytesfqcn
+      //geoObject.country = body.geobytesinternet
+      geoObject.city = body.data.geo.city
+      geoObject.region = body.data.geo.region_name
+      geoObject.country = body.data.geo.country_name
+      geoObject.cn = body.data.geo.country_code
+      //console.log(geoObject)
       callback(undefined, geoObject)
     }
   })
